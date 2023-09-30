785efb30f1f1df5a23c78f12a248b5dfe0a7d32d
Isabella <sorianoisabella28@gmail.com>
2023-09-30 18:52:02 -0500

    creamos ApiClient en el package remote. Aqui creamos una instancia del CharacterService

--- app/src/main/java/com/example/logincompose/data/remote/ApiClient.kt
+++ app/src/main/java/com/example/logincompose/data/remote/ApiClient.kt
@@ -1,12 +1,13 @@
 package com.example.logincompose.data.remote
 
+//kotlin->object
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
     const val API_BASE_URL="https://plain-marbled-muskox.glitch.me/"  //siempre hasta un "/"
-
     private var userService: UserService?=null
 
     fun getUserService(): UserService {
@@ -22,4 +23,30 @@ object ApiClient {
         }
         return userService as UserService
     }
+
+
+    ///CREO UNA INSTANCIA DEL SERVICIO
+
+    const val RICK_AND_MORTY_API_BASE_URL="https://rickandmortyapi.com/api/"  //siempre hasta un "/"
+    private var characterService: CharacterService?=null
+
+    fun getCharacterService(): CharacterService {
+
+        if(characterService ==null){
+
+            val retrofit=Retrofit
+                .Builder()
+                .baseUrl(RICK_AND_MORTY_API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            characterService =retrofit.create(CharacterService::class.java)
+        }
+        return characterService as CharacterService
+    }
+
+
+
+
+
+
 }
