25c03c8e0e0aba018c6e1dc4c73f2a9e553a17e7
Isabella <sorianoisabella28@gmail.com>
2023-09-30 21:24:57 -0500

    crear metodo en el repositorio para buscar un character por id

--- app/src/main/java/com/example/logincompose/repository/CharacterRepository.kt
+++ app/src/main/java/com/example/logincompose/repository/CharacterRepository.kt
@@ -1,5 +1,6 @@
 package com.example.logincompose.repository
 
+import com.example.logincompose.data.model.Character
 import com.example.logincompose.data.model.CharacterResponse
 import com.example.logincompose.data.remote.ApiClient
 import com.example.logincompose.data.remote.CharacterService
@@ -9,6 +10,8 @@ import retrofit2.Callback
 import retrofit2.Response
 
 //kotlin->class
+
+//VA A TRAER LOS DATOS, debe tener lomismo que el servicio!!!!!!!!!!
 class CharacterRepository(
   private val characterService:CharacterService =ApiClient.getCharacterService()
 
@@ -43,6 +46,26 @@ class CharacterRepository(
     })
   }
 
+
+  fun getById(id:String,callback: (Result<Character>) -> Unit){
+
+    val getById=characterService.getById(id)
+
+    //la variable
+    getById.enqueue(object: Callback<Character>{
+
+      override fun onResponse(call: Call<Character>, response: Response<Character>) {
+
+        if(response.isSuccessful){
+          callback(Result.Success(data=response.body()!!))
+        }
+      }
+
+      override fun onFailure(call: Call<Character>, t: Throwable) {
+        callback(Result.Error(message=t.localizedMessage!!))
+      }
+    })
+  }
 
 
 }
