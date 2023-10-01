693b4b21b172e569c756c477f2c771f9dd1056de
Isabella <sorianoisabella28@gmail.com>
2023-09-30 20:04:30 -0500

    implementar CharacterList, solo mostrar el nombre por ahora.
    Hacer la llamada en MainAvtivity

--- app/src/main/java/com/example/logincompose/MainActivity.kt
+++ app/src/main/java/com/example/logincompose/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.example.logincompose.ui.characterlist.CharacterList
+import com.example.logincompose.ui.characterlist.CharacterListViewModel
 import com.example.logincompose.ui.theme.LoginComposeTheme
 import com.example.logincompose.ui.login.Login
 import com.example.logincompose.ui.signup.SignUp
@@ -12,12 +14,12 @@ import com.example.logincompose.ui.signup.SignUpViewModel
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel:SignUpViewModel by viewModels()
+        val viewModel:CharacterListViewModel by viewModels()
         setContent {
             LoginComposeTheme {
                 // A surface container using the 'background' color from the theme
 
-                SignUp(viewModel)
+                CharacterList(viewModel)
             }
         }
     }
--- app/src/main/java/com/example/logincompose/ui/characterlist/CharacterList.kt
+++ app/src/main/java/com/example/logincompose/ui/characterlist/CharacterList.kt
@@ -1,14 +1,37 @@
 package com.example.logincompose.ui.characterlist
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items            //!!!!!!!
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue                 //!!!!!!!
 import androidx.compose.runtime.livedata.observeAsState
+import com.example.logincompose.data.model.Character
 
 //otlin->file
 @Composable
 fun CharacterList(viewModel: CharacterListViewModel){
 
     //instancia del view model
-    val characters=viewModel.characters.observeAsState(listOf())
+    val characters:List<Character> by viewModel.characters.observeAsState(listOf())
+    viewModel.getAll() //trae toda la info de los character
 
+    LazyColumn{
+        items(characters){character->
+            CharacterCard(character)
+        }
+    }
+}
+
+@Composable
+fun CharacterCard(character: Character) {
+
+    Card {
+        Row {
+            Text(text=character.name)
+        }
+    }
 
 }
