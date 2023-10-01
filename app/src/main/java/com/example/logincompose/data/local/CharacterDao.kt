76b2389d0823119bc3531377c1b60ce75a079e6e
Isabella <sorianoisabella28@gmail.com>
2023-09-30 22:03:34 -0500

    creo la interfaz DAO (metodos para interactuar con la base de datos local que creare). Corrijo algunas cosas de la entidad

---
+++ app/src/main/java/com/example/logincompose/data/local/CharacterDao.kt
@@ -0,0 +1,33 @@
+package com.example.logincompose.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.logincompose.data.model.Character
+import kotlinx.coroutines.selects.select
+
+//kotlin->interface
+
+//DAO (Data Access Object): Un DAO en Room es una interfaz o clase abstracta
+// que define los métodos para interactuar con la base de datos.
+// Los métodos del DAO permiten realizar operaciones como insertar,
+// actualizar, eliminar y consultar datos en la base de datos.
+@Dao
+interface CharacterDao {
+
+    @Insert  //funcion de insertar
+    fun save(character:CharacterEntity)
+
+    @Query("select * from characters where:id")
+    //debe encontrar un characterEntity, sino encuentra, es nulo(?)
+    fun getById(id:Int):CharacterEntity?
+
+    @Delete
+    fun delete(character: CharacterEntity)
+
+    @Query("select * from characters")
+    fun getAll(id:Int):List<CharacterEntity>
+
+
+}
--- app/src/main/java/com/example/logincompose/data/local/CharacterEntity.kt
+++ app/src/main/java/com/example/logincompose/data/local/CharacterEntity.kt
@@ -1,10 +1,18 @@
 package com.example.logincompose.data.local
 
+//kotlin->data class
+
+// (Entity): Una entidad en Room representa una tabla dentro de la base de datos.
+// Cada instancia de la entidad corresponde a una fila en la tabla.
+// En términos sencillos, una entidad es como una clase que representa una
+// estructura de datos en tu base de datos.
+
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName="characters")
 data class CharacterEntity(
 
     //solo quiero guardar el dato del id
-    val id:Int
+    @PrimaryKey val id:Int
 )
